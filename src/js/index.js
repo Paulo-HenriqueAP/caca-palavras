af8bb4941fb23a617274fdcd2c4bce5585146144
Paulo Henrique  ΛP <123279522+Paulo-HenriqueAP@users.noreply.github.com>
2025-02-16 12:29:01 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,130 @@
+const alp = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i",
+    "j", "k", "l", "m", "n", "o", "p", "q", "r",
+    "s", "t", "u", "v", "w", "x", "y", "z", "ç",
+    'á', 'à', 'â', 'ã', 'é', 'ê', 'í', 'ó', 'ô',
+    'õ', 'ú'
+    //'á', 'à', 'â', 'ã', 'é', 'è', 'ê', 'ẽ', 'í',
+    //'ì', 'î', 'ĩ', 'ó', 'ò', 'ô', 'õ', 'ú', 'ù',
+    //'û', 'ũ', 'ý', 'ỳ', 'ŷ', 'ỹ'
+];
+
+const words = "src/words/br-utf8.txt";
+
+let findIt = [
+
+];
+
+let arrayTXT;
+let word = alp[Math.floor(Math.random() * alp.length)];
+let validWords = 0;
+let listOfWords;
+
+createTR()
+
+function createTR() {
+    let Hsize = 0;
+
+    while (Hsize < 10) {
+        const tr = document.createElement("tr");
+        tr.id = "tr" + Hsize;
+        Hsize++;
+        document.getElementById("bodyTable").appendChild(tr);
+
+        if (Math.floor(Math.random() * 999) % 2 === 0) {
+            randomTD(tr.id)
+        } else if (validWords < 5) {
+            createValidWord(tr.id)
+            validWords++
+        } else {
+            randomTD(tr.id)
+        }
+    };
+    transformL("MAIUSCULA");
+
+    setTimeout(function () {
+        console.log(findIt)
+        findIt.sort()
+        randomLi();
+        document.getElementById("main").classList.remove("hidden")
+    }, 100)
+};
+
+function createValidWord(idName) {
+    fetch(words)
+        .then((res) => res.text())
+        .then((data) => {
+            (arrayTXT = data.split(/\r?\n/));
+
+            text = arrayTXT[Math.floor(Math.random() * arrayTXT.length)];
+
+            word = text;
+
+            findIt.push(word)
+
+            if (Math.floor(Math.random() * 999) % 2 === 0) {
+                //console.log(word + " | antes")
+                word = word.split("").reverse().join("")
+                //console.log(word + " | depois")
+            }
+
+            startPoint = Math.floor(Math.random() * word.length)
+
+            let beforeWord = "";
+
+            for (i = 0; i < startPoint; i++) {
+                beforeWord += alp[Math.floor(Math.random() * alp.length)];
+            }
+
+            beforeWord += word;
+            word = beforeWord
+
+            if (word.length < 23) {
+                for (i = word.length; i < 23; i++) {
+                    word += alp[Math.floor(Math.random() * alp.length)];
+                };
+            }
+
+            Array.from(word).forEach(singleLetter => {
+                const td = document.createElement("td");
+                td.textContent = singleLetter
+                document.getElementById(idName).appendChild(td);
+                //td.classList.add("valido")
+            });
+        });
+
+};
+
+function randomLi() {
+    console.log(findIt)
+
+    findIt.forEach((function (name) {
+        li = document.createElement("li");
+        li.textContent = name;
+        document.getElementById("wordsLI").appendChild(li)
+    }))
+}
+
+function randomTD(idName) {
+    for (let i = 0; i < 23; i++) {
+        w = alp[Math.floor(Math.random() * alp.length)]
+        word += w;
+
+        const td = document.createElement("td");
+
+        td.textContent = alp[Math.floor(Math.random() * alp.length)];
+
+        document.getElementById(idName).appendChild(td);
+        //td.classList.add("invalido")
+    };
+    //console.log(word)
+};
+
+function transformL(makeIt) {
+    document.body.classList.remove("upper", "low")
+    makeIt == "MAIUSCULA" ? document.body.classList.add("upper") : document.body.classList.add("low");
+};
+
+document.addEventListener("keydown", function (event) {
+    event.key == "F4" ? window.print() : null;
+})
